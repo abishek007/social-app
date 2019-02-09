1402e6d2bdeb31f431157d1a1f4e74b021dc8174
abishek.d <abisheksrv@gmail.com>
2019-02-09 19:32:48 +0530

    added changes

--- src/component/index.js
+++ src/component/index.js
@@ -8,33 +8,33 @@ import "./styles.css"
 class App extends Component {
   state = {}
 
-	render() {
-		console.log("this.props", this.props)
-		const { card } = this.props
+  render() {
+    console.log("this.props", this.props)
+    const { card } = this.props
     return (
       <div className="card-container">
-			{card.map(ele => (
-				<div className="card">
-					<div className="vote-button"></div>
-					<div className="content-sec">
-						<div className="info-sec">
-							<p className="title">Title:</p>
-							<p className="value">{ele.title}</p>
-						</div>
-						<div className="info-sec">
-							<p className="title">SubTitle:</p>
-							<p className="value">{ele.subtitle}</p>
-						</div>
-						<div className="info-sec">
-							<p className="title">Author:</p>
-							<p className="value">{ele.author}</p>
-						</div>
-					</div>
-				</div>
-				))
-			}
-			</div>
-		)
+      {card.map(ele => (
+        <div className="card">
+          <div className="vote-button"></div>
+          <div className="content-sec">
+            <div className="info-sec">
+              <p className="title">Title:</p>
+              <p className="value">{ele.title}</p>
+            </div>
+            <div className="info-sec">
+              <p className="title">SubTitle:</p>
+              <p className="value">{ele.subtitle}</p>
+            </div>
+            <div className="info-sec">
+              <p className="title">Author:</p>
+              <p className="value">{ele.author}</p>
+            </div>
+          </div>
+        </div>
+        ))
+      }
+      </div>
+    )
   }
 }
 
--- src/container/index.js
+++ src/container/index.js
@@ -3,17 +3,17 @@ import { bindActionCreators } from "redux"
 import { likePost, upVote, downVote } from "./actions"
 
 function mapStateToProps(state) {
-	console.log("state", state)
-	const { card } = state.socialAppReducer || []
+  console.log("state", state)
+  const { card } = state.socialAppReducer || []
   return { card }
 }
   
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({
-		likePost,
-		upVote,
-		downVote,
-	}, dispatch)
+  return bindActionCreators({
+    likePost,
+    upVote,
+    downVote,
+  }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)
